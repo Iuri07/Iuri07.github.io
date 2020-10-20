f2f7c701f9f9485a45ec0003c1680e7f4ae0b9e0
Iuri07 <iuricostermani@hotmail.com>
2020-10-20 20:16:20 -0300

    First version of homepage

---
+++ sketch.js
@@ -0,0 +1,56 @@
+let c;
+let t = 0;
+let r;
+let speed = 0.01
+
+function drawCircle(max_r, color, t ) {
+  beginShape();
+  strokeWeight( 5 );
+  noFill();
+  stroke( color );
+  for ( let angle = 0; angle < TWO_PI; angle += 0.1 ) {
+    let xoff = cos( angle ) + 1;
+    let yoff = sin( angle ) + 1;
+    let r = map( noise( xoff, yoff, t ), 0, 1, max_r * .97, max_r );
+    let circle_x = r * cos( angle );
+    let circle_y = r * sin( angle );
+    vertex( circle_x, circle_y );
+  }
+  endShape( CLOSE );
+}
+
+function setup() {
+  createCanvas( windowWidth, windowHeight );
+
+}
+
+function draw() {
+  r = Math.min( width, height ) / 2 - 50;
+
+  blendMode( BLEND )
+  background( 20 );
+
+  colorMode( RGB, 255, 255, 255, 1 );
+  blendMode( SCREEN );
+  noFill();
+  translate( width / 2, height / 2 );
+
+  c = color( 255, 0, 0, .7 );
+  translate( 0, 0 )
+  drawCircle( r, c, t );
+
+  rotate( PI / 3 )
+  c = color( 0, 255, 0, .7 );
+  drawCircle(  r, c, t );
+
+  rotate( PI / 3 )
+  c = color( 0,0,255, .7 );
+  drawCircle( r, c, t );
+
+  t += speed
+
+}
+
+function windowResized() {
+  resizeCanvas( windowWidth, windowHeight );
+}
